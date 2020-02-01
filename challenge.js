a090d8627ec1fb34045594af1c1bd69b921a95ed
melonybox <grandmadcow@gmail.com>
2020-01-31 21:29:20 -0500

    Done

--- challenge.js
+++ challenge.js
@@ -2,22 +2,35 @@ const paraString = "If you want to jumpstart the process of talking to us about 
 
 const findMinChar = (stringInput) => {
   const stringArr = stringInput.split("")
-  let stringObj = {}
-  let uniqArr = []
+  let stringObj = []
 
   stringArr.forEach(char => {
     stringObj[char] === undefined ? stringObj[char] = 1 : stringObj[char]++
   })
 
-  for (let i = 0; Object.keys(stringObj).length > i; i++) {
+  let stringSort = Object.entries(stringObj)
+  let stringSorted = []
+
+  stringSort.sort((a,b) => a[1] - b[1])
+
+  stringSort.forEach(item => {
+    stringSorted[item[0]] = item[1]
+  })
+
+  for (let i = 0; Object.keys(stringSorted).length > i; i++) {
     let x = []
-    uniqArr.push(x)
-    for (let j = i; Object.keys(stringObj).length > j; j++) {
-      x.push(Object.values(stringObj)[j])
+    let uniqArr = []
+    for (let j = i; Object.keys(stringSorted).length > j; j++) {
+      x.push(Object.values(stringSorted)[j])
+      uniqArr.push(Object.keys(stringSorted)[j])
+      if (x.length > 1) {
+        const amountTotal = x.reduce((total, num) => total + num)
+        if (amountTotal >= 50) {
+          return console.log(uniqArr)
+        }
+      }
     }
   }
-
-  return console.log(uniqArr)
 }
 
 findMinChar(paraString)
